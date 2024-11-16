2ce5f75d0edc16fd24faf9f9cde9c9dd2250ead2
juan arango <juanarangozapata35@gmail.com>
2024-11-16 05:45:58 -0500

    agrega colores a los typos de pokemon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 import { Link } from "react-router-dom";
 import imgUser from "../assets/user-3-fill.svg";
 import imgSearch from "../assets/search-fill.svg";
-import imgStar from "../assets/star-fill.svg";
+import imgStar from "../assets/star.svg";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 const Header = () => {
     const { logout } = useContext(AuthContext);
   return (
-    <div className="bg-blue-500 py-2">
-      <ul className="flex justify-end items-center mr-5">
+    <div className="bg-blue-500 py-2 -ml-10 -mr-10">
+      <ul className="flex justify-end items-center mr-10">
         <li className="mr-5">
           <Link to="/profile">
             <img src={imgUser} alt="imgUser"  className="w-10"/>
@@ -21,7 +21,7 @@ const Header = () => {
         </li>
         <li  className="mr-5">
           <Link to="/favorites">
-            <img src={imgStar} alt="imgStar" className="w-[35px]" />
+            <img src={imgStar} alt="imgStar" className="w-[35px] text-gray-500"  />
           </Link>
         </li>
         <li  className="mr-5">
--- src/components/PokemonData.jsx
+++ src/components/PokemonData.jsx
@@ -1,66 +1,120 @@
-import React from 'react';
+import React from "react";
 
 import { RiStarFill } from "react-icons/ri";
 import { LiaRulerVerticalSolid } from "react-icons/lia";
 import { GiWeightScale } from "react-icons/gi";
-import Button from './Button';
-function formatId(id){
-    id=String(id);
-    return id.length < 2 ? `#00${id}` : id.length < 3 ? `#0${id}`: `#${id}`
+import Button from "./Button";
+import { clsx } from "clsx";
+import styled, { css } from "styled-components";
+
+const typeColors = {
+  grass: "#74CB48",
+  poison: "#A43E9E",
+  fire: "#F57D31",
+  water: "#6493EB",
+  electric: "#F9CF30",
+  ground: "#DEC16B",
+  flying: "#A891EC",
+  bug: "#A7B723",
+  normal: "#AAA67F",
+  fighting: "#C12239",
+  psychic: "#FB5584",
+  rock: "#B69E31",
+  ice: "#9AD6DF",
+  dragon: "#7037FF",
+  ghost: "#705693",
+  dark: "#75574C",
+  steel: "#B7B9D0",
+  fairy: "#E69EAC",
+  unknown: "#68A090",
+  shadow: "#000",
+};
+
+const ContentTypes = styled.div`
+  display: flex;
+  gap: 30px;
+  margin-bottom: 40px;
+`;
+const StyleType = styled.p`
+  ${({ type }) => css`
+    background: ${typeColors[type]};
+    font-weight: bold;
+    font-size: 16px;
+    color: white;
+    border-radius: 10px;
+    padding: 2px 8px;
+    font-size: 10px;
+    font-weight: 700;
+    line-height: 20px;
+  `}
+`; 
+function formatId(id) {
+  id = String(id);
+  return id.length < 2 ? `#00${id}` : id.length < 3 ? `#0${id}` : `#${id}`;
 }
-const PokemonData = ({dataPokemon, onAddFavorite,onRemoveFavorite,isFavorite}) => {
-
-const regularContent=(
+const PokemonData = ({
+  dataPokemon,
+  onAddFavorite,
+  onRemoveFavorite,
+  isFavorite,
+}) => {
+  const regularContent = (
     <>
-<RiStarFill color="E0E0E0" /> Agrega a Favoritos
+      <RiStarFill color="E0E0E0" /> Agrega a Favoritos
     </>
-)
+  );
 
-const favoriteContent=(
+  const favoriteContent = (
     <>
-      <RiStarFill   color="#F2C94C" /> Quitar de Favoritos
+      <RiStarFill color="#F2C94C" /> Quitar de Favoritos
     </>
-)
-
-
+  );
 
   return (
-    <div className='flex flex-col items-center'>
-        <h1 className='text-2xl font-bold'>{dataPokemon.name}</h1> 
-        <p className='text-lg font-bold'>{formatId(dataPokemon.id)}</p>
-        <img 
-        className='w-[200px] h-[200px]'
-        src={dataPokemon.sprites.other["official-artwork"].front_default} alt={dataPokemon.name} />
-        <ul className='flex gap-2 mb-5'>
-            {dataPokemon.types.map((element)=>(
-                <li key={element.name}>{element.type.name}</li>
-            ))}
-        </ul>
-        <div className='flex gap-5'>
-            <div className='flex flex-col'>
-                <div className='flex justify-center items-center gap-6'>
-                <GiWeightScale/>
-                {dataPokemon.weight /10} kg 
-                </div>
-              
-                <p className='text-center font-bold'>Peso</p>
-            </div>
-            <span className='w-0.5 h-10 bg-gray-400'></span>
-            <div className='flex flex-col'>
-            <div className='flex justify-center items-center gap-6'>
-                <LiaRulerVerticalSolid/>
-                {dataPokemon.height /10} m
-                </div>
-                <p className='text-center font-bold'>Altura</p>
-            </div>
-        </div>
-        <Button 
-        className='mt-5 w-[200px] flex justify-around items-center'
-        onClick={()=>isFavorite ? onRemoveFavorite(dataPokemon) : onAddFavorite(dataPokemon)}>
-            {isFavorite ? favoriteContent : regularContent}
-        </Button>
-    </div>
-  )
-}
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold mt-4">{dataPokemon.name}</h1>
+      <p className="text-lg font-bold">{formatId(dataPokemon.id)}</p>
+      <img
+        className="w-[200px] h-[200px]"
+        src={dataPokemon.sprites.other["official-artwork"].front_default}
+        alt={dataPokemon.name}
+      />
+      <ContentTypes >
+        {dataPokemon.types.map((element, index) => {
+          
+          return <StyleType key={index} type={element.type.name}>{element.type.name}</StyleType>;
+        })}
+      </ContentTypes>
+      <div className="flex gap-5">
+        <div className="flex flex-col">
+          <div className="flex justify-center items-center gap-6">
+            <GiWeightScale />
+            {dataPokemon.weight / 10} kg
+          </div>
 
-export default PokemonData
+          <p className="text-center font-bold mt-4">Peso</p>
+        </div>
+        <span className="w-0.5 h-16 bg-gray-400"></span>
+        <div className="flex flex-col">
+          <div className="flex justify-center items-center gap-6">
+            <LiaRulerVerticalSolid />
+            {dataPokemon.height / 10} m
+          </div>
+          <p className="text-center font-bold mt-4">Altura</p>
+        </div>
+      </div>
+      <Button
+        className="mt-5 w-[200px] flex justify-around items-center"
+        onClick={() =>
+          isFavorite
+            ? onRemoveFavorite(dataPokemon)
+            : onAddFavorite(dataPokemon)
+        }
+      >
+        {isFavorite ? favoriteContent : regularContent}
+      </Button>
+    </div>
+  );
+};
+
+export default PokemonData;
